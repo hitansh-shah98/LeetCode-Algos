f6e1b5aa0a5d1fa695e4d7b20a7280124b2dec89
hitansh-shah98 <hitansh1998@gmail.com>
2022-04-26 13:21:28 +0530

    done

---
+++ 2130.Maximum-Twin-Sum-of-Linked List.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode root =head;
+        
+          List<Integer> al = new ArrayList<Integer>();
+        while(root!=null){
+          
+            al.add(root.val);
+            root=root.next;
+        }
+        int length = al.size();
+        //System.out.println("List size: " + al.size() + " " + al);
+        int max =0;
+        for(int i=0;i<length/2;i++){
+            max = Math.max(max , al.get(i) + al.get(length-i-1));
+        }
+      
+        return max;
+    }
+}
+
+
+// faster soln using only linked list
+class Solution {
+    public int pairSum(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+        if (head.next == null) {
+            return head.val;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        int sum = Integer.MIN_VALUE;
+        while (slow != null) {
+            sum = Math.max(slow.val + fast.val, sum);
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return sum;
+    }
+    
+    public ListNode reverse(ListNode node) {
+        if (node == null) {
+            return null;
+        }
+        ListNode current = node;
+        ListNode previous = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
